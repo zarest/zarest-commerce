8478ce83544c853e88b2fe64a3d080554c4cb267
Meysam Abolghasemi <meysamabl@gmail.com>
2014-10-21 13:15:17 +0800

    Internationalization setup

--- app/Global.java
+++ app/Global.java
@@ -2,8 +2,14 @@ import com.avaje.ebean.Ebean;
 import models.Category;
 import play.Application;
 import play.GlobalSettings;
+import play.api.mvc.Handler;
+import play.libs.F;
 import play.libs.Yaml;
+import play.mvc.Action;
+import play.mvc.Http;
+import play.mvc.Result;
 
+import java.lang.reflect.Method;
 import java.util.List;
 import java.util.Map;
 
@@ -32,6 +38,24 @@ public class Global extends GlobalSettings {
 //            // Insert tasks
 //            Ebean.save(all.get("tasks"));
 
+        }
+    }
+
+    //@Override
+    public Action onRequest(final Http.Request request, final Method actionMethod) {
+
+        if (request.host().equals("fa.meysamabl.com")
+                && (request.cookie("PLAY_LANG") == null || !request.cookie("PLAY_LANG").value().equals("fa"))) {
+            return new Action.Simple() {
+                @Override
+                public F.Promise<Result> call(Http.Context ctx) throws Throwable {
+                    ctx.changeLang("fa");
+                    return delegate.call(ctx);
+                }
+            };
+
+        } else {
+            return super.onRequest(request, actionMethod);
         }
     }
 }
--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,14 +2,43 @@ package controllers;
 
 import models.Category;
 import play.*;
+import play.data.DynamicForm;
+import play.data.Form;
+import play.i18n.Lang;
 import play.mvc.*;
 
 import views.html.*;
 
+//@With(Language.class)
 public class Application extends Controller {
+
 
     public static Result index() {
         return ok(index.render("Your new application is ready.", Category.find.all()));
     }
 
+    public static Lang getCurrentLang() {
+        return Http.Context.current().lang();
+    }
+
+    public static Result changeLocale() {
+        DynamicForm requestData = Form.form().bindFromRequest();
+        String locale = requestData.get("locale");
+        Logger.info("My Locale: " + locale);
+        changeLang(locale);
+        if (request().hasHeader(REFERER)) {
+            String refererURL = request().getHeader(REFERER);
+            Logger.info("My URL: " + refererURL);
+            return redirect(refererURL);
+        } else {
+            Logger.info("GOING BACK HOME: " + routes.Application.index().absoluteURL(request()));
+            return redirect(routes.Application.index().absoluteURL(request()));
+        }
+
+    }
+
+
+    public static Result productPage() {
+        return redirect("/products");
+    }
 }
